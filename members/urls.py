94c38140dd8e9c397d42d05640069f61a1bd925d
Aturner060 <aturner060@gmail.com>
2023-12-09 20:31:22 +0100

    Added functions: about, contacts, register, login, change_password to members/views.py. Connected to members/urls.py.

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('about/', views.about),
+    path('contacts/', views.contacts),
+    path('register/', views.register),
+    path('login/', views.login),
+    path('forgot_password/', views.forgot_password),
 ]
--- members/views.py
+++ members/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Main page')
+    return HttpResponse('Главная страница')
+
+
+def about(request):
+    return HttpResponse('Информация о сайте')
+
+
+def contacts(request):
+    return HttpResponse('Телефон, почта, соц.сети')
+
+
+def register(request):
+    return HttpResponse('Форма заполнения анкеты')
+
+
+def login(request):
+    return HttpResponse('Форма входа на сайт')
+
+
+def forgot_password(request):
+    return HttpResponse('Форма смены пароля')
